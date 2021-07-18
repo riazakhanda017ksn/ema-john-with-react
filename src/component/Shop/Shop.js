7df15f5b97c79449ad64cd3348da21bf1d8c69c7
riazakhanda017ksn <riazkahanda@gmail.com>
2021-07-18 08:35:52 -0700

    product added

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -12,15 +12,15 @@ const Shop = () => {
   const [products, setProducts] = useState(first10);
   const [cart, setCart] = useState([]);
   const handleProduct = (product) => {
-    let addToBeAdded = product.key;
-    const sameProduct = cart.find((pd) => pd.key === addToBeAdded);
-    let count = 0;
+    let toBeAdded = product.key;
+    const sameProduct = cart.find((pd) => pd.key === toBeAdded);
+    let count = 1;
     let newCart;
     if (sameProduct) {
       count = sameProduct.quantity + 1;
       sameProduct.quantity = count;
-      const other = cart.filter((pd) => pd.key !== addToBeAdded);
-      newCart = [...other, product];
+      const others = cart.filter((pd) => pd.key !== toBeAdded);
+      newCart = [...others, product];
     } else {
       product.quantity = 1;
       newCart = [...cart, product];
